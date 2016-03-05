d0784fa6d1b0b37ab12a49afff62c66729b2e5e4
Shubham <shubhammuttepawar1@gmail.com>
2016-03-05 20:02:44 +0530

    US10 : Adding the missing enums

---
+++ casm-backend/src/main/java/org/casm/domain/enums/CrateType.java
@@ -0,0 +1,8 @@
+package org.casm.domain.enums;
+
+/**
+ * Created by Shubham on 3/5/2016.
+ */
+public enum CrateType {
+    CARD_BOARD, PLASTIC
+}
---
+++ casm-backend/src/main/java/org/casm/domain/enums/Packaging.java
@@ -0,0 +1,9 @@
+package org.casm.domain.enums;
+
+/**
+ * Created by Shubham on 3/5/2016.
+ */
+public enum Packaging {
+    GLASS_BOTTLE,
+    PLASTIC_BOTTLE,
+}
